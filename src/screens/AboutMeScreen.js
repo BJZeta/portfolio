cc24db48149950f114a09502470b91c1114c35ed
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-03-22 16:36:59 -0700

    editing style for about screen

--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -5,35 +5,30 @@ import profile from "../img/profile.jpg";
 const AboutMeScreen = () => {
   return (
     <div className="aboutscreen">
-      <Row lg={3} className="justify-content-center">
-        <Col>
-          <img alt="Brandon Unzueta" className="profile-pic" src={profile} />
+      <Row lg={3} md={1} className="justify-content-center">
+        <Col className="profile-pic">
+          <img alt="Brandon Unzueta" src={profile} />
         </Col>
-        <Col >
-          <Col>
-            <h3>About Me</h3>
-            <p>
-              My name is Brandon Unzueta, and I am a Full Stack React Developer.
-              My coding skills include{" "}
-              <strong>
-                HTML, CSS, Javascript, Node, Express, MongoDB, Next.js, and
-                React
-              </strong>
-            </p>
-          </Col>
-          <Row>
-            <Col>
-              <h3>Contact Information</h3>
-              <p>Brandon Unzueta</p>
-              <p>18014 1/2 Arline Ave</p>
-              <p>Artesia, CA. 90701</p>
-              <p>{`(562)788-0363`}</p>
-              <p>unzueta.brandon@gmail.com</p>
-            </Col>
-            <Col>
-              <Button className="resume-btn">Resume <i className="fas fa-file-alt"/></Button>
-            </Col>
-          </Row>
+        <Col>
+          <h3>About Me</h3>
+          <p>
+            My name is Brandon Unzueta, and I am a Full Stack React Developer.
+            My skills in software include{" "}
+            <strong>
+              HTML, CSS, Javascript, Node, Express, MongoDB, Next.js, and React
+            </strong>
+          </p>
+        </Col>
+        <Col className="text-center">
+          <h3>Contact Information</h3>
+          <p>Brandon Unzueta</p>
+          <p>18014 1/2 Arline Ave</p>
+          <p>Artesia, CA. 90701</p>
+          <p>{`(562)788-0363`}</p>
+          <p>unzueta.brandon@gmail.com</p>
+          <Button className="resume-btn">
+            Resume <i className="fas fa-file-alt" />
+          </Button>
         </Col>
       </Row>
     </div>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,6 @@ import React from "react";
 const HomeScreen = () => {
   return (
     <div className="homescreen text-center">
-      <h4>Hello, My Name is</h4>
       <h1>Brandon Unzueta</h1>
       <h4>React Developer</h4>
     </div>
